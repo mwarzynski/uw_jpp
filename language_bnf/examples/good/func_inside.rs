4972f8dacfd4da6c7c73798652424fb6758a7995
Mateusz Warzyński <mathew.warzynski@gmail.com>
2018-04-09 14:49:20 +0200

    Improvements in grammar #6 (structs), examples - language_bnf

--- language_bnf/examples/good/func_inside.rs
+++ language_bnf/examples/good/func_inside.rs
@@ -3,10 +3,6 @@ struct Rectangle {
   y: i32
 }
 
-fn rectangle_area(r: Rectangle) : i32 {
-  return r.x * r.y;
-}
-
 fn rectangle_new(x,y: i32) : Rectangle {
   r: Rectangle;
   r.x = x;
@@ -14,6 +10,10 @@ fn rectangle_new(x,y: i32) : Rectangle {
 }
 
 fn main() : i32 {
+  fn rectangle_area(r: Rectangle) : i32 {
+    return r.x * r.y;
+  }
+
   r = rectangle_new(10, 20);
   print(rectangle_area(r));
 
