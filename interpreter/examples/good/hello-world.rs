9bbde1676799a984a83d1b0d0789910da876d5c2
Mateusz Warzyński <mathew.warzynski@gmail.com>
2018-05-08 11:42:50 +0200

    Add declaring more variables to hello-world example - interpretre

--- interpreter/examples/good/hello-world.rs
+++ interpreter/examples/good/hello-world.rs
@@ -1,3 +1,7 @@
 fn main() -> {
-    print("Hello, World");
+    h: str = "Hello, World";
+    i: int = 1333333777;
+    f: float = 1.3234;
+    b: bool = true;
+    print(h, " ", i, " ", f, " ", b);
 }
