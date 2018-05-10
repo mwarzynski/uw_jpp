8f90f539a8dc1e76aef7944edd104872956c2834
Mateusz Warzyński <mathew.warzynski@gmail.com>
2018-05-10 14:43:54 +0200

    Fix struct example - interpreter

--- interpreter/examples/good/struct.rs
+++ interpreter/examples/good/struct.rs
@@ -8,15 +8,14 @@ fn rectangle_area(r: Rectangle) -> int {
   return r.x * r.y;
 }
 
-fn rectangle_new(x,y: int) -> Rectangle {
+fn rectangle_new(x: int, y: int) -> Rectangle {
   r: Rectangle;
   r.x = x;
   r.y = y;
+  return r;
 }
 
-fn main() -> int {
-  r = rectangle_new(10, 20);
+fn main() -> {
+  r: Rectangle = rectangle_new(10, 20);
   print(rectangle_area(r));
-
-  return 0;
 }
