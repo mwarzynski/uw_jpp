d040aef8d60ee09b2d4ea307808dca445da0e3b0
Mateusz Warzyński <mathew.warzynski@gmail.com>
2018-05-10 15:56:15 +0200

    Fix script for updating grammar - interpreter

--- interpreter/update-grammar.sh
+++ interpreter/update-grammar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp grammar.cf build/
-docker exec -ti lucid_wozniak "bnfc -m -haskell grammar.cf"
+docker exec -ti lucid_wozniak bnfc -m -haskell grammar.cf
 docker exec -ti lucid_wozniak make
 cp build/AbsGrammar.hs src/
 sudo chown -R mwarzynski build
