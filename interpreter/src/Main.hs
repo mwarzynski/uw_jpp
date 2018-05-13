8ca0ba1c8030e60fc947a853a87f2531dd014512
Mateusz Warzyński <mathew.warzynski@gmail.com>
2018-05-13 23:19:12 +0200

    Change parsing arguments - interpreter

--- interpreter/src/Main.hs
+++ interpreter/src/Main.hs
@@ -18,9 +18,6 @@ import Types (typesAnalyze)
 import Interpreter (interpret)
 
 
-runFile :: FilePath -> IO ()
-runFile f = readFile f >>= run
-
 run :: String -> IO ()
 run text = let tokens = myLexer text in case pProgram tokens of
              Bad e      -> do
@@ -36,24 +33,10 @@ run text = let tokens = myLexer text in case pProgram tokens of
                               Left e -> hPutStrLn stderr $ "Runtime error: " ++ e
                               _        -> return ()
 
-usage :: IO ()
-usage = do
-  putStrLn $ unlines
-    [ "The Interpreter."
-    , ""
-    , "usage: Call with one of the following argument combinations:"
-    , "  --help, -h      Display this help message."
-    , "  (no arguments)  Interpret stdin."
-    , "  -f (file)       Interpret content of given file."
-    ]
-  exitSuccess
-
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    ["-h"] -> usage
-    ["--help"] -> usage
     [] -> hGetContents stdin >>= run
-    "-f":fs -> mapM_ (runFile) fs
+    (file:_) -> readFile file >>= run
 
