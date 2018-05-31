51a2ec1e3de99eaa5f392e6b1020c0a6202b96c8
Mateusz Warzyński <mathew.warzynski@gmail.com>
2018-05-31 11:39:37 +0200

    Add scratch solution file - prolog

---
+++ prolog/mw371854.pl
@@ -0,0 +1,12 @@
+% Mateusz Warzyński, 371854
+
+use_module(library(lists)).
+% Also (np. member/2, append/3, length/2)
+
+% jestWyborem(+AEgraf, -Graf)
+% jestDFS(+Graf, -Lista)
+% jestADFS(+AEgraf, -Lista)
+% jestADFS1(+AEgraf, -Lista)
+
+main :- write("Hello, World"),nl,fail.
+
