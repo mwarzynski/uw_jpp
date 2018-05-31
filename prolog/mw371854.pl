d0a690addbb63416b01a1f769c6ba1b84e38f911
Mateusz Warzyński <mathew.warzynski@gmail.com>
2018-05-31 18:10:59 +0200

    Implement checking conditions for A,E verticles - prolog

--- prolog/mw371854.pl
+++ prolog/mw371854.pl
@@ -1,21 +1,72 @@
 % Mateusz Warzyński, 371854
 
 use_module(library(lists)).
-
 % Also (np. member/2, append/3, length/2)
 
 % jestWyborem(+AEGraf, -Graf)
 % Prawda jeśli Graf jest wyborem AEGraf.
 % Gdy dla AEgraf istnieje wiele wyborów, predykat powinien
 % odnosić wielokrotnie sukces, przynajmniej raz dla każdego wyboru.
-jestWyborem(AEGraf, Graf) :- true.
+jestWyborem(AEGraf, Graf) :- jestWyboremA(AEGraf, Graf), jestWyboremE(AEGraf, Graf).
 
+% jestWyboremA(+AEGraf, -Graf)
+% Prawda jeśli dla każdego v ∈ Va, i każdego <v, v'> ∈ R zachodzi <v, v'> ∈ S.
+jestWyboremA(_, []).
+jestWyboremA(AEGraf, [V | Vs]) :-
+    jestWyboremAWierzcholek(AEGraf, V),
+    jestWyboremA(AEGraf, Vs).
+
+% jestWyboremAWierzcholek(Ws, V)
+% Prawda jeśli graf złożony tylko z wierzchołka V spełnia założenia
+% dotyczące A-wierzchołków jeśli całkowitym grafem jest Ws.
+jestWyboremAWierzcholek(_, V) :- wierzcholekE(V).
+jestWyboremAWierzcholek([W | _], V) :-
+    wierzcholekA(W),
+    wierzcholkiTeSame(W, V),
+    jestWyboremASprawdzWierzcholki(W, V).
+jestWyboremAWierzcholek([_ | Ws], V) :-
+    jestWyboremAWierzcholek(Ws, V).
+jestWyboremASprawdzWierzcholki([W, WType | Ws], [V, VType | Vs]) :-
+    W = V,
+    WType = a, VType = a,
+    listaPorownaj(Ws, Vs).
+
+% jestWyboremE(+AEGraf, -Graf)
+% Prawda jeśli dla każdego v ∈ Ve istnieje <v, v'> ∈ R dla pewnego v', to istnieje
+% dokładnie jedno <v, v''> ∈ R, takie że <v, v''> ∈ S.
+jestWyboremE(_, []).
+jestWyboremE(AEGraf, [V | Vs]) :-
+    jestWyboremEWierzcholek(AEGraf, V),
+    jestWyboremE(AEGraf, Vs).
+
+jestWyboremEWierzcholek(_, V) :- wierzcholekA(V).
+jestWyboremEWierzcholek([W | _], V) :-
+    wierzcholekE(W),
+    wierzcholkiTeSame(W, V),
+    jestWyboremESprawdzWierzcholki(W, V).
+jestWyboremEWierzcholek([_ | Ws], V) :-
+    jestWyboremEWierzcholek(Ws, V).
+jestWyboremESprawdzWierzcholki([W, WType | Ws], [V, VType | Vs]) :-
+    W = V,
+    WType = e, VType = e,
+    length(Ws, Wlength),
+    Wlength > 0,
+    length(Vs, Vlength),
+    Vlength > 0,
+    listaIloczyn(Ws, Vs, X),
+    X = 1.
+
+jestWyboremESprawdzWierzcholki([W, WType | Ws], [V, VType | _]) :-
+    W = V,
+    WType = e, VType = e,
+    length(Ws, Wlength),
+    Wlength = 0.
 
 % jestDFS(+Graf, -Lista)
 % Prawda gdy Lista jest lista identyfikatorów
 % wierzchołków kolejno odwiedzanych przez algorytm przechodzenia
 % grafu Graf w głąb przy przejściu startujacym z pierwszego wierzchołka tego grafu
-jestDFS(Graf, Lista) :- true.
+% jestDFS(Graf, Lista) :- true.
 
 
 % jestADFS(+AEgraf, -Lista)
@@ -23,7 +74,7 @@ jestDFS(Graf, Lista) :- true.
 % wierzchołków kolejno odwiedzanych przez algorytm przechodzenia w głab przy przejści
 % przez pewien graf będacy wyborem z AEgraf. W definicji tego predykatu należy jawnie
 % zbudować reprezentacje pewnego wyboru AE-grafu AEgraf.
-jestADFS(AEGraf, Lista) :- true.
+% jestADFS(AEGraf, Lista) :- true.
 
 
 % jestADFS1(+AEGraf, -Lista)
@@ -31,5 +82,28 @@ jestADFS(AEGraf, Lista) :- true.
 % wierzchołków kolejno odwiedzanych przez algorytm przechodzenia w głab przy przejściu
 % przez pewien graf będacy wyborem z AEgraf. W trakcie obliczania tego predykatu
 % nie może jawnie być budowana reprezentacja wyboru AE-grafu AEgraf.
-jestADFS1(AEGraf, Lista) :- true.
+% jestADFS1(AEGraf, Lista) :- true.
+
+% wierzcholkiTeSame(V, W)
+% Prawda, jeśli obydwa wierzchołki są takie same.
+% Predykat nie sprawdza sąsiadów.
+wierzcholkiTeSame([V, Vt | _], [W, Wt | _]) :-
+    V  = W,
+    Vt = Wt.
+
+% listaPorownaj(A, B)
+% Prawda, jeśli wszystkie elementy listy A są również w liście B.
+listaPorownaj([], _).
+listaPorownaj([A|As], B) :-
+    member(A, B),
+    listaPorownaj(As, B).
+
+listaIloczyn([], _, X) :- X is 0.
+listaIloczyn(_, [], X) :- X is 0.
+listaIloczyn([A|As], B, X) :-
+   listaIloczyn(As, B, Xx),
+   ( member(A, B) -> X is Xx + 1 ; X is Xx ).
+
+wierzcholekA([_, Typ | _]) :- Typ = a.
+wierzcholekE([_, Typ | _]) :- Typ = e.
 
