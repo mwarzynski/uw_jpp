596a19e1c592da939cac8922f5c102d1364fe67a
Mateusz Warzyński <mathew.warzynski@gmail.com>
2018-06-01 17:06:57 +0200

    Clean up - prolog

--- prolog/mw371854.pl
+++ prolog/mw371854.pl
@@ -58,20 +58,16 @@ jestDFS2(Graf, [Id | Stos], Odwiedzone, Wynik) :-
 jestADFS(AEGraf, Lista) :-
     jestDFS(AEGraf, Lista),
     listaDoGraf(AEGraf, Lista, Wybor),
+    write(AEGraf), write("\n"),
+    write(Wybor), write("\n"),
     jestWyborem(AEGraf, Wybor).
-
 
 % jestADFS1(+AEGraf, -Lista)
 % Prawda gdy Lista jest lista identyfikatorów
 % wierzchołków kolejno odwiedzanych przez algorytm przechodzenia w głab przy przejściu
 % przez pewien graf będacy wyborem z AEgraf. W trakcie obliczania tego predykatu
 % nie może jawnie być budowana reprezentacja wyboru AE-grafu AEgraf.
-% jestADFS1(AEGraf, Lista) :- true.
-
-
-wierzcholkiIstnieja(G, [Id, Ids]) :-
-    wierzcholekOID(G, Id, _),
-    wierzcholkiIstnieja(G, Ids).
+% jestADFS1(AEGraf, Lista) :-
 
 % wierzcholekOID(Vs, Id, Wynik)
 % Ustawia na Wynik wierzchołek o podanym Id z listy wierzchołków Vs.
@@ -81,53 +77,42 @@ wierzcholekOID([V | _], Id, R) :-
     R = V.
 wierzcholekOID([_| Vs], Id, R) :- wierzcholekOID(Vs, Id, R).
 
-% wierzcholkiTeSame(V, W)
-% Prawda, jeśli obydwa wierzchołki są takie same.
-% Predykat nie sprawdza sąsiadów.
-wierzcholkiTeSame([V, Vt | _], [W, Wt | _]) :-
-    V  = W,
-    Vt = Wt.
-
 wierzcholekID([V | _], Id) :- Id = V.
+wierzcholekTyp([_, Typ | _], T) :- T = Typ.
 wierzcholekSasiedzi([_, _ | Vs], Sasiedzi) :- Sasiedzi = Vs.
 
-wierzcholekTyp([_, Typ | _], T) :- T = Typ.
+listaDoGraf([], _, Graf) :- Graf = [].
+listaDoGraf([H|T], Lista, Graf) :-
+    wierzcholekID(H, HId),
+    not(member(HId, Lista)),
+    listaDoGraf(T, Lista, Graf).
+listaDoGraf([H|T], Lista, Graf) :-
+    wierzcholekID(H, HId),
+    member(HId, Lista),
+    listaDoGraf(T, Lista, Gg),
+    wierzcholekTyp(H, Htyp),
+    wierzcholekSasiedzi(H, Hs),
+    wierzcholekPrzytnij(HId, Htyp, Hs, NV),
+    append([NV], Gg, Graf).
 
-grafDoLista([], Lista) :- Lista = [].
-grafDoLista([G|Gs], Lista) :-
-    grafDoLista(Gs, L),
-    wierzcholekID(G, Id),
-    append(L, [Id], Ll),
-    Lista = Ll.
-
-listaDoGraf(_, [], Graf) :- Graf = [].
-listaDoGraf(AEGraf, [H|T], Graf) :-
-    wierzcholekOID(AEGraf, H, V),
-    listaDoGraf(AEGraf, T, Gg),
-    append(Gg, [V], G),
-    Graf = G.
-
-% listaPorownaj(A, B)
-% Prawda, jeśli wszystkie elementy listy A są również w liście B.
-listaPorownaj([], _).
-listaPorownaj([A|As], B) :-
-    member(A, B),
-    listaPorownaj(As, B).
-
-listaIloczyn([], _, X) :- X is 0.
-listaIloczyn(_, [], X) :- X is 0.
-listaIloczyn([A|As], B, X) :-
-   listaIloczyn(As, B, Xx),
-   ( member(A, B) -> X is Xx + 1 ; X is Xx ).
+wierzcholekPrzytnij(VId, a, Vs, V) :-
+    append([VId, a], Vs, V).
+wierzcholekPrzytnij(VId, e, [], V) :-
+    V = [VId, e].
+wierzcholekPrzytnij(VId, e, [Vs | _], V) :-
+    append([VId, e], Vs, V).
 
 listaOdwroc([], []).
 listaOdwroc([H|T],Z) :-
     listaOdwroc(T,Z1),
     append(Z1, [H], Z).
 
-listaPermutuj([X|Y],Z) :- listaPermutuj(Y,W), listaZamien(X,Z,W).
 listaPermutuj([],[]).
+listaPermutuj([X|Y],Z) :-
+    listaPermutuj(Y,W),
+    listaZamien(X,Z,W).
 
 listaZamien(X, [X|R], R).
-listaZamien(X, [F|R], [F|S]) :- listaZamien(X,R,S).
+listaZamien(X, [F|R], [F|S]) :-
+    listaZamien(X,R,S).
 
